ef9adeec1abaf17690dd43bb2c7bb43def52dc48
jangho <jhc@dgu.edu>
2015-11-16 23:27:57 +0900

    use pytest on travis

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,4 +1,21 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+
+if __name__ == '__main__':  #7
+    unittest.main(warnings='ignore')  #8
