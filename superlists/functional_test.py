ccf6bced23e87ec532384814408de4b603b97733
jangho <jhc@dgu.edu>
2015-11-16 23:44:54 +0900

    use pytest test

--- superlists/functional_test.py
+++ superlists/functional_test.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
 
@@ -12,10 +12,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        # Edith has heard about a cool new online to-do app. She goes
-        # to check out its homepage
         self.browser.get('http://localhost:8000')
 
 
-if __name__ == '__main__':  #7
-    unittest.main(warnings='ignore')  #8
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
